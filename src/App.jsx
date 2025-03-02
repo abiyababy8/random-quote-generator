0be56de22dcfc5518970e468b9315f9fbc9d0107
abiyababy8 <abiyababy16@gmail.com>
2025-03-02 20:01:34 +0530

    'random-quotes-generator'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from "./components/Nav"
+import QuoteGenerator from "./components/QuoteGenerator"
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+      <QuoteGenerator/>
+      
+    </>
+  )
+}
+
+export default App
